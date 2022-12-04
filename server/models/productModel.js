b3410bf3ee42f4bbdacae7c77cf0703a4563f875
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2022-12-04 17:55:09 +0000

    working on user and product Models

---
+++ server/models/productModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    nom: {
+      type: String,
+      required: [true, 'votre nom svp'],
+      lowercase: true,
+    },
+    categorie: {
+      type: String,
+      enum: ['sneaker', 'culotte', 'T-shirt', 'complet'],
+      required: [true, 'une catégorie est necessaire pour chaque article'],
+    },
+    quantite: {
+      type: Number,
+      default: 10,
+      max: 100,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    note: {
+      type: Number,
+      default: 1,
+      min: 1,
+      max: 5,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+    timeseries: true,
+  }
+);
+
+module.exports = mongoose.model('Product', productSchema);
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,14 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema(
   {
-    nom: String,
-    prenom: String,
-    dateNaiss: Date,
-    email: String,
+    nom: { type: String, required: [true, 'votre nom SVP !'] },
+    prenom: { type: String, required: [true, 'votre prenom SVP !'] },
+    dateNaiss: {
+      type: Date,
+      required: [true, 'votre de naissance SVP !'],
+      validate: {
+        validator: function (date) {
+          return (
+            date &&
+            new Date(Date.now()).getFullYear() -
+              new Date(date).getFullYear() >
+              16
+          );
+        },
+        message: `vous n'avez pas l'age requis pour pouvoir effectuer des achats sur ce site`,
+      },
+    },
+    email: {
+      type: String,
+      validate: [validator.isEmail, `votre E-mail n'est pas valide`],
+      required: [true, 'Veillez entrer votre E-mail !'],
+    },
     password: String,
-    panier: [{ type: mongoose.Schema.objectId }],
-    envies: [{ type: mongoose.Schema.objectId }],
+    panier: [{ type: mongoose.Schema.objectId, populate: true }],
+    envies: [{ type: mongoose.Schema.objectId, populate: true }],
   },
-  { timestamps: true }
+  { timestamps: true, timeseries: true }
 );
+
+module.exports = mongoose.model('User', userSchema);
